5dcbbbdcda1f15b4411502e4a0777365c7a9847b
theemohanraj <theemohanraj@gmail.com>
2020-12-21 12:53:26 +0530

    master updated

--- src/Add.java
+++ src/Add.java
@@ -4,7 +4,7 @@ public class Add {
 	public static void main(String[] args) {
 		System.out.println(21);
 		
-		System.out.println("Sample git");
+		System.out.println("Sample git in master");
 		
 		
 	}
